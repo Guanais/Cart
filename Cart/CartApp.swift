4512d1156f5576fbc8dd27ce84ca595a3b498416
Kaio <kaioaguanais@gmail.com>
2020-10-16 13:09:00 -0700

    Initial Commit

---
+++ Cart/CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartApp.swift
+//  Cart
+//
+//  Created by Kaio Guanais on 2020-10-16.
+//
+
+import SwiftUI
+
+@main
+struct CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
